9869309a0bdaba995bf0dab1e6c679ee14383c90
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-17 13:34:18 -0800

    validates that rating is an integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :content_body, length: { in: 50..250 }
-  validates :rating, numericality: { less_than_or_equal_to_5: true }
+  validates :rating, numericality: { only_integer: true,  less_than_or_equal_to_5: true }
 end
