479c5d31fe1526e78005b41675e4f575d07e2992
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-17 13:32:44 -0800

    add validation that rating is a number that is less than or equal to 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :content_body, length: { in: 50..250 }
+  validates :rating, numericality: { less_than_or_equal_to_5: true }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,6 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_length_of :content_body}
+  it { should validate_numericality_of :rating }
 
 end
